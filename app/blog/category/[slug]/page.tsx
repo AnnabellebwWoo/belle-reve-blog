44ea3905a10a9475beb5ccdc86fc7112691e27b9
Annabelle <annabelle.bw.woo@gmail.com>
2026-09-13 14:01:07 -0400

    cloudflare configs

--- app/blog/category/[slug]/page.tsx
+++ app/blog/category/[slug]/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { getPostsByCategory } from "../../../../lib/utils";
+import { getPostsByCategory, getAllCategories } from "../../../../lib/utils";
 import BlogCard from "../../../../components/BlogCard/BlogCard";
 import styles from "./page.module.css";
+
+export async function generateStaticParams() {
+  const categories = await getAllCategories();
+  return categories.map((category) => ({
+    slug: category,
+  }));
+}
 
 export default async function CategoryPage({
   params,
